b6d5533f744043faa9a73ccf9efc2b8eba555481
AbdurRahman4991 <engrabdurrahman4991@gmail.com>
2024-01-22 09:29:32 +0600

    update

--- app/helpers/helper.php
+++ app/helpers/helper.php
@@ -1,34 +1,17 @@
 <?php
-use App\Events\SendNotification;
-use App\Notifications\UserNotification;
-
 function ResponseMethod($status, $message)
 {
     return response()->json([
         'status' => $status,
         'message' => $message,
-    ], 200);
+    ]);
 }
 
-function ResponseErrorMessage($status, $message)
+function ResponseErroMethod($status, $message)
 {
     return response()->json([
         'status' => $status,
         'message' => $message,
-    ], 404);
+    ]);
 }
-
-// =====================NOTIFICATION==================//
-
-function sendNotification($message, $data)
-{
-    try {
-        event(new SendNotification($message, $data));
-        \Notification::send($data, new UserNotification($data));
-        return response()->json(['success' => true, 'msg' => 'Notification Added'], 200);
-    } catch (\Exception $e) {
-        return response()->json(['success' => false, 'msg' => $e->getMessage()]);
-    }
-}
-
-// ======================TOKENT=============//
+?>
