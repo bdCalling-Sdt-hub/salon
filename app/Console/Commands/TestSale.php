1c6fb0dac56a52690a5b89b26fd461046f940ba4
AbdurRahman4991 <engrabdurrahman4991@gmail.com>
2024-02-03 10:01:17 +0600

    update status code message

---
+++ app/Console/Commands/TestSale.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class TestSale extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:test-sale';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
