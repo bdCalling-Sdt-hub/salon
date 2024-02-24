fe8cdd88cdec2a0da01544d972ea573efeadeb98
AbdurRahman4991 <engrabdurrahman4991@gmail.com>
2024-02-24 11:39:22 +0600

    update notification

--- app/Notifications/AdminNotification.php
+++ app/Notifications/AdminNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class UserNotification extends Notification
+class AdminNotification extends Notification
 {
     use Queueable;
 
@@ -16,6 +16,9 @@ class UserNotification extends Notification
     protected $message;
     protected $description;
 
+    /**
+     * Create a new notification instance.
+     */
     public function __construct($message, $description, $user)
     {
         $this->message = $message;
@@ -23,6 +26,11 @@ class UserNotification extends Notification
         $this->user = $user;
     }
 
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
     public function via($notifiable): array
     {
         return ['database'];
--- app/helpers/helper.php
+++ app/helpers/helper.php
@@ -1,5 +1,7 @@
 <?php
 use App\Events\SendNotification;
+use App\Notifications\AdminNotification;
+use App\Notifications\SalonNotification;
 use App\Notifications\UserNotification;
 use Illuminate\Support\Facades\Notification;
 
@@ -40,4 +42,35 @@ function sendNotification($message = null, $description = null, $data = null, $p
         return response()->json(['success' => false, 'msg' => $e->getMessage()]);
     }
 }
+
+function providerNotification($message = null, $description = null, $data = null, $payment = null)
+{
+    try {
+        event(new SendNotification($message, $data));
+        Notification::send($data, new SalonNotification($message, $description, $data));
+        return response()->json([
+            'success' => true,
+            'msg' => 'Notification Added',
+            'data' => $data,
+        ]);
+    } catch (\Exception $e) {
+        return response()->json(['success' => false, 'msg' => $e->getMessage()]);
+    }
+}
+
+function adminNotification($message = null, $description = null, $data = null, $payment = null)
+{
+    try {
+        event(new SendNotification($message, $data));
+        Notification::send($data, new AdminNotification($message, $description, $data));
+        return response()->json([
+            'success' => true,
+            'msg' => 'Notification Added',
+            'data' => $data,
+        ]);
+    } catch (\Exception $e) {
+        return response()->json(['success' => false, 'msg' => $e->getMessage()]);
+    }
+}
+
 ?>
